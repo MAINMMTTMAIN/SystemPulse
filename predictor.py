45a9867e3d78c3ae61b1b9928442c11826e40485
MohammadMahdi TorkTatari <146365848+MAINMMTTMAIN@users.noreply.github.com>
2026-02-06 02:18:35 +0330

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -3,17 +3,18 @@ from sklearn.linear_model import LinearRegression
 import numpy as np
 
 def predict_usage(data_log, metric='cpu'):
-    if len(data_log) < 10:  # حداقل داده
+    if len(data_log) < 10:  
         return 0
     
     df = pd.DataFrame(data_log)
-    df['timestamp'] = pd.to_datetime(df['time']).astype(int) / 10**9  # unix time برای رگرسیون
-    X = df['timestamp'].values[-60:].reshape(-1, 1)  # آخرین ۶۰ داده
+    df['timestamp'] = pd.to_datetime(df['time']).astype(int) / 10**9  
+    X = df['timestamp'].values[-60:].reshape(-1, 1)  
     y = df[metric].values[-60:]
     
     model = LinearRegression()
     model.fit(X, y)
     
-    future_time = df['timestamp'].iloc[-1] + 60  # ۱ دقیقه آینده
+    future_time = df['timestamp'].iloc[-1] + 60  
     pred = model.predict([[future_time]])
+
     return pred[0]
